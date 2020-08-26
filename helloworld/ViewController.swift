44a67990196076b5f80606ac509c4e30e3fca100
kentarokimura <nekmik527@gmail.com>
2020-08-26 17:47:41 +0900

    Initial Commit

---
+++ helloworld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by 木村謙太郎 on 2020/08/26.
+//  Copyright © 2020 kentaro kimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
